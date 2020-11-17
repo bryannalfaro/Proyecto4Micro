275424504a2461485cd05273f1c2f3fee3bd0ea8
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-11-16 21:18:25 -0600

    primera estructura

--- Wallis.cu
+++ Wallis.cu
@@ -2,14 +2,27 @@
 #include <cuda_runtime.h>
 
 __global__ void
-serieWallis(float *var, int *var)
+serieWallis(float *convergencia, int *vectorN, int limite)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
+    
     
 
 }
 
 int main(void){
+
+    int valorN =1000;
+
+    //Reserva en el host
+    int *host_vectorN= (int *)malloc(sizeof(int));
+    float *host_vectorValor= (float *)malloc(sizeof(float));
+
+    //Llenado con los valores de N
+    for(int i = 1; i <= valorN; i++)
+     {
+         host_vectorN[i-1]=i;
+     }
 
     
 
